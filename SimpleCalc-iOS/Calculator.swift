93b5b054cb08ee814cad7a2452eabfd590cf1180
Duncan Andrew <duncanandrew@live.com>
2017-04-19 12:36:03 -0700

    updated styles to work on all screens and orientations

---
+++ SimpleCalc-iOS/Calculator.swift
@@ -0,0 +1,13 @@
+//
+//  Calculator.swift
+//  SimpleCalc-iOS
+//
+//  Created by Duncan Andrew on 4/19/17.
+//  Copyright © 2017 Duncan Andrew. All rights reserved.
+//
+
+import UIKit
+
+class Calculator: NSObject {
+
+}
